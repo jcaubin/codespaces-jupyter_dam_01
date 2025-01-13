4f88d121fb0917411d1595caa6bc86874af21584
jcaubin <jcaubin@gmail.com>
2025-01-13 17:04:26 +0100

    carga en bbdd local y sisaulizacion separadas

--- scripts/run_notebook_report.sh
+++ scripts/run_notebook_report.sh
@@ -3,11 +3,11 @@
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 echo "Current Time : $current_time"
 
-file_name=pato_meteo_madrid
-new_fileName=$file_name.$current_time.html
-echo "FileName: " "$new_fileName"
+notebook_name=pato_meteo_data
+report_name=$notebook_name.$current_time.md
+echo "Report: " "$report_name"
 
-report_path=/home/jcaubin/code/github/codespaces-jupyter_dam_01/reports/$new_fileName 
-notebook_path=/home/jcaubin/code/github/codespaces-jupyter_dam_01/notebooks/pato_meteo_seaborn.ipynb
+report_path=/home/jcaubin/code/github/codespaces-jupyter_dam_01/reports/$report_name 
+notebook_path=/home/jcaubin/code/github/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
 
-/home/jcaubin/code/github/codespaces-jupyter_dam_01/.venv/bin/jupyter nbconvert --to html --execute --no-input --output $report_path $notebook_path
+/home/jcaubin/code/github/codespaces-jupyter_dam_01/.venv/bin/jupyter nbconvert --to markdown --execute --no-input --output $report_path $notebook_path
