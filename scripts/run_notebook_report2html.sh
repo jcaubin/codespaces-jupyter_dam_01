46e651aae64f190d874aaf8ec1f8323ffd5f6582
jcaubin <jcaubin@gmail.com>
2025-01-24 13:58:29 +0100

    nuevo script para generar el html

--- scripts/run_notebook_report2html.sh
+++ scripts/run_notebook_report2html.sh
@@ -3,17 +3,6 @@
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 echo "Current Time : $current_time"
 
-notebook_name=pato_meteo_data
-report_name=$notebook_name.$current_time.md
-echo "Report: " "$report_name"
-
-report_path=/home/jcaubin/code/codespaces-jupyter_dam_01/reports/$report_name 
-notebook_path=/home/jcaubin/code/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
-
-/home/jcaubin/code/codespaces-jupyter_dam_01/.venv/bin/jupyter nbconvert --to markdown --execute --no-input --output $report_path $notebook_path
-
-####################
-
 notebook_name=pato_meteo_plotly_report
 report_name=index.html
 echo "Report: " "$report_name"
