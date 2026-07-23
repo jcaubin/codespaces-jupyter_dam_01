190b5060caec8d7a61acefe181e13104d5e73f25
jcaubin <jcaubin@gmail.com>
2026-07-23 16:17:02 +0200

    rutas locales

--- scripts/run_notebook_report.sh
+++ scripts/run_notebook_report.sh
@@ -7,10 +7,10 @@ notebook_name=pato_meteo_data
 report_name=$notebook_name.$current_time.md
 echo "Report: " "$report_name"
 
-report_path=/home/jcaubin/code/codespaces-jupyter_dam_01/reports/$report_name 
-notebook_path=/home/jcaubin/code/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
+report_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/$report_name 
+notebook_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
 
-/home/jcaubin/code/codespaces-jupyter_dam_01/.venv/bin/jupyter nbconvert --to markdown --execute --no-input --output $report_path $notebook_path
+/home/jcaubin/codigo/codespaces-jupyter_dam_01/.venv/bin/jupyter nbconvert --to markdown --execute --no-input --output $report_path $notebook_path
 
 ####################
 
@@ -18,21 +18,21 @@ notebook_name=pato_calair_data
 report_name=$notebook_name.$current_time.md
 echo "Report: " "$report_name"
 
-report_path=/home/jcaubin/code/codespaces-jupyter_dam_01/reports/$report_name 
-notebook_path=/home/jcaubin/code/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
+report_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/$report_name 
+notebook_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
 
-/home/jcaubin/code/codespaces-jupyter_dam_01/.venv/bin/jupyter nbconvert --to markdown --execute --no-input --output $report_path $notebook_path
+/home/jcaubin/codigo/codespaces-jupyter_dam_01/.venv/bin/jupyter nbconvert --to markdown --execute --no-input --output $report_path $notebook_path
 
 ####################
 #precipitaciones2
 echo "precipitaciones2"
-/home/jcaubin/code/codespaces-jupyter_dam_01/.venv/bin/python /home/jcaubin/code/codespaces-jupyter_dam_01/src/write_plotly.py
-sudo cp /home/jcaubin/code/codespaces-jupyter_dam_01/reports/index.html /var/www/html/index.html
+/home/jcaubin/codigo/codespaces-jupyter_dam_01/.venv/bin/python /home/jcaubin/codigo/codespaces-jupyter_dam_01/src/write_plotly.py
+sudo cp /home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/index.html /var/www/html/index.html
 
 #temperaturas
 echo "temperaturas"
-/home/jcaubin/code/codespaces-jupyter_dam_01/.venv/bin/python /home/jcaubin/code/codespaces-jupyter_dam_01/src/write_plotly_temp.py
-sudo cp /home/jcaubin/code/codespaces-jupyter_dam_01/reports/temperaturas.html /var/www/html/temperaturas.html
+/home/jcaubin/codigo/codespaces-jupyter_dam_01/.venv/bin/python /home/jcaubin/codigo/codespaces-jupyter_dam_01/src/write_plotly_temp.py
+sudo cp /home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/temperaturas.html /var/www/html/temperaturas.html
 
 #####################################
 
@@ -40,10 +40,10 @@ notebook_name=pato_meteo_plotly_report
 report_name=meteo.html
 echo "Report: " "$report_name"
 
-report_path=/home/jcaubin/code/codespaces-jupyter_dam_01/reports/$report_name 
-notebook_path=/home/jcaubin/code/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
+report_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/$report_name 
+notebook_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
 
-/home/jcaubin/code/codespaces-jupyter_dam_01/.venv/bin/jupyter nbconvert --to html --execute --no-input --output $report_path $notebook_path
+/home/jcaubin/codigo/codespaces-jupyter_dam_01/.venv/bin/jupyter nbconvert --to html --execute --no-input --output $report_path $notebook_path
 sudo cp $report_path /var/www/html/$report_name
 
 
@@ -52,9 +52,9 @@ notebook_name=pato_precipitaciones_html
 report_name=precipitaciones.html
 echo "Report: " "$report_name"
 
-report_path=/home/jcaubin/code/codespaces-jupyter_dam_01/reports/$report_name 
-notebook_path=/home/jcaubin/code/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
+report_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/$report_name 
+notebook_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
 
-/home/jcaubin/code/codespaces-jupyter_dam_01/.venv/bin/jupyter nbconvert --to html --execute --no-input --output $report_path $notebook_path
+/home/jcaubin/codigo/codespaces-jupyter_dam_01/.venv/bin/jupyter nbconvert --to html --execute --no-input --output $report_path $notebook_path
 sudo cp $report_path /var/www/html/$report_name
 
