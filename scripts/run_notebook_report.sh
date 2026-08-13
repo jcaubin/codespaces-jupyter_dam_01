c0a4ffcf25f5ae80c8af83052ffa955e13050a21
jcaubin <jcaubin@gmail.com>
2026-08-13 10:36:24 +0200

    nueva página radiacion solar

--- scripts/run_notebook_report.sh
+++ scripts/run_notebook_report.sh
@@ -1,9 +1,10 @@
-#ejecuta el nb 
-#DATOS METEO
-#DESCARGA Y GUARDA LOS DATOS METEO EN DDB
+#LANZADERA  PARA EJECUTAR NOTEBOOKS Y GENERAR INFORMES EN HTML Y MARKDOWN
+
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 echo "Current Time : $current_time"
 
+#########################################
+# DESCARGA Y GUARDA LOS DATOS METEO EN DDB
 notebook_name=pato_meteo_data
 report_name=$notebook_name.$current_time.md
 echo "Report: " "$report_name"
@@ -26,7 +27,11 @@ notebook_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/notebooks/$notebook
 
 /home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/jupyter nbconvert --to markdown --execute --no-input --output $report_path $notebook_path
 
+
+
+
 ####################
+
 #precipitaciones2
 echo "precipitaciones2"
 /home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/codigo/codespaces-jupyter_dam_01/src/write_plotly.py
@@ -37,36 +42,13 @@ echo "temperaturas"
 /home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/codigo/codespaces-jupyter_dam_01/src/write_plotly_temp.py
 cp /home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/temperaturas.html /var/www/html/meteo/temperaturas.html
 
-#####################################
-
-notebook_name=pato_meteo_plotly_report
-report_name=meteo.html
-echo "Report: " "$report_name"
-
-report_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/$report_name 
-notebook_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
-
-/home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/jupyter nbconvert --to html --execute --no-input --output $report_path $notebook_path
-cp $report_path /var/www/html/meteo/$report_name
-
-
-#precipitaciones
-notebook_name=pato_precipitaciones_html
-report_name=precipitaciones.html
-echo "Report: " "$report_name"
-
-report_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/$report_name 
-notebook_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
-
-/home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/jupyter nbconvert --to html --execute --no-input --output $report_path $notebook_path
-cp $report_path /var/www/html/meteo/$report_name
-
-
-#general, escribe directamente en var-wwww
 echo "general"
 /home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/codigo/codespaces-jupyter_dam_01/src/write_plotly_general.py
 
 echo "ozono"
 /home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/codigo/codespaces-jupyter_dam_01/src/write_plotly_ozono.py
+
+echo "radiacion"
+/home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/codigo/codespaces-jupyter_dam_01/src/write_plotly_radiacion.py
 
 
---
+++ src/write_plotly_radiacion.py
@@ -0,0 +1,57 @@
+#grafica de temperaturas
+
+import plotly.express as px
+import plotly.io as pio
+import plotly.graph_objects as go
+from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
+import duckdb
+
+
+def main():
+        magnitud=88
+        page_name ='radiacion'
+
+        with duckdb.connect('/home/jcaubin/datos/duck_test.db') as conn:
+                parametro_nombre = conn.sql(f"""
+                        SELECT DISTINCT PARAMETRO
+                        FROM duck_test.main.CALAIR
+                        WHERE MAGNITUD = {magnitud}
+                        """).fetchone()[0]
+                df = conn.sql(f"""
+                        select TS, dia,  h, estacion_desc,valor 
+                        from v_CALAIR 
+                        where magnitud = {magnitud}
+                        and validez = 'V'
+                        and date_diff ('hour',TS, current_localtimestamp()) < (25)
+                        order by  TS, estacion_desc
+                """).df()      
+                fig = px.box(df, x= 'TS', y = 'VALOR', title = f'Distribución diaria - {parametro_nombre}', hover_name='ESTACION_DESC',  
+                        color_discrete_sequence=["#8484A7"], template='plotly_dark')
+                df = conn.sql(f"""
+                        select TS, dia,  h, estacion_desc,valor 
+                        from v_CALAIR 
+                        where magnitud = {magnitud}
+                        and validez = 'V'
+                        and date_diff ('hour',TS, current_localtimestamp()) < (7*24)+1
+                        order by  TS, estacion_desc
+                """).df()   
+                fig2 = px.box(df, x= 'TS', y = 'VALOR', title = f'Distribución semanal - {parametro_nombre}', hover_name='ESTACION_DESC',  
+                        color_discrete_sequence=["#8484A7"], template='plotly_dark')
+                
+        plotly_jinja_data = {
+                "fig":fig.to_html(full_html=False, include_plotlyjs=False , default_width='100%'), 
+                "date" : datetime.now().strftime('%Y-%m-%d %H:%M:%S '),
+                "title": f"Magnitud {parametro_nombre}",
+                "fig2":fig2.to_html(full_html=False, include_plotlyjs=False, default_width='100%'),
+                }
+
+        environment = Environment(loader=FileSystemLoader("/home/jcaubin/codigo/codespaces-jupyter_dam_01/templates/"))
+        template = environment.get_template("plotly.html")
+        output_html_path=f"/var/www/html/meteo/{page_name}.html"
+        with open(output_html_path, "w", encoding="utf-8") as output_file:
+                output_file.write(template.render(plotly_jinja_data))
+
+if __name__=='__main__' :
+        main()
