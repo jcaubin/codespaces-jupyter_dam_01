131a537d578f834a9d2ea285ecac00515648fca1
jcaubin <jcaubin@gmail.com>
2026-08-30 18:38:12 +0200

    script para crear la bbdd integrado, tablas y vistas

--- sql/001_schema.sql
+++ sql/001_schema.sql
@@ -43,3 +43,25 @@ CREATE INDEX IF NOT EXISTS idx_calair_ts
 
 CREATE INDEX IF NOT EXISTS idx_calair_magnitud
     ON calair (magnitud, validez);
+
+
+ --vistas
+CREATE OR REPLACE VIEW v_calair AS
+SELECT
+    provincia,
+    municipio,
+    estacion,
+    magnitud,
+    punto_muestreo,
+    ano,
+    mes,
+    dia,
+    h,
+    valor,
+    validez,
+    estacion_desc,
+    altitud,
+    parametro,
+    fx_data,
+    make_timestamp(ano, mes, dia, h, 0, 0.0) AS ts
+FROM calair;   
