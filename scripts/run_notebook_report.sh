9703dcfefc4d3631bc9d03ba41346a5daba41f67
jcaubin <jcaubin@gmail.com>
2026-09-01 20:09:03 +0200

    rf del scrpt bash lanzadera

--- scripts/run_notebook_report.sh
+++ scripts/run_notebook_report.sh
@@ -3,52 +3,36 @@
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 echo "Current Time : $current_time"
 
-#########################################
-# DESCARGA Y GUARDA LOS DATOS METEO EN DDB
-notebook_name=pato_meteo_data
-report_name=$notebook_name.$current_time.md
-echo "Report: " "$report_name"
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd -- "${SCRIPT_DIR}/.." && pwd)"
+VENV_PYTHON="${PROJECT_ROOT}/.venv/bin/python"
 
-report_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/$report_name 
-notebook_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
+########################################
+# DESCARGA Y GUARDA LOS DATOS METEO Y CALAIR EN DDB USANDO UN SCRIPT DE PYTHON
+echo "descarga y guarda los datos meteo y calair en ddb"
+echo "ejecutando script: ${VENV_PYTHON}  /home/jcaubin/code/codespaces-jupyter_dam_01/src/pato_calair_data.py"
+"${VENV_PYTHON}" "${PROJECT_ROOT}/src/pato_calair_data.py"
 
-/home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/jupyter nbconvert --to markdown --execute --no-input --output $report_path $notebook_path
-
-####################
-#DATOS CALAIR
-#DESCARGA Y GUARDA LOS DATOS DE CALIDAD EN DDB
-
-notebook_name=pato_calair_data
-report_name=$notebook_name.$current_time.md
-echo "Report: " "$report_name"
-
-report_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/$report_name 
-notebook_path=/home/jcaubin/codigo/codespaces-jupyter_dam_01/notebooks/$notebook_name.ipynb
-
-/home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/jupyter nbconvert --to markdown --execute --no-input --output $report_path $notebook_path
-
-
-
-
-####################
+#######################################
+#INFORMES HTML
+echo "INFORMES DE MAGNITUDES"
+"${VENV_PYTHON}" "${PROJECT_ROOT}/src/write_plotly_magnitud.py"
 
 #precipitaciones2
-echo "precipitaciones2"
-/home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/codigo/codespaces-jupyter_dam_01/src/write_plotly.py
-cp /home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/index.html /var/www/html/meteo/index.html
+#echo "precipitaciones2"
+#/home/jcaubin/code/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/code/codespaces-jupyter_dam_01/src/write_plotly.py
+#cp /home/jcaubin/code/codespaces-jupyter_dam_01/reports/index.html /var/www/html/meteo/index.html
 
 #temperaturas
-echo "temperaturas"
-#/home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/codigo/codespaces-jupyter_dam_01/src/write_plotly_temp.py
-#cp /home/jcaubin/codigo/codespaces-jupyter_dam_01/reports/temperaturas.html /var/www/html/meteo/temperaturas.html
+#echo "temperaturas"
+#/home/jcaubin/code/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/code/codespaces-jupyter_dam_01/src/write_plotly_temp.py
+#cp /home/jcaubin/code/codespaces-jupyter_dam_01/reports/temperaturas.html /var/www/html/meteo/temperaturas.html
 
 
-echo "ozono"
-#/home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/codigo/codespaces-jupyter_dam_01/src/write_plotly_ozono.py
+#echo "ozono"
+#/home/jcaubin/code/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/code/codespaces-jupyter_dam_01/src/write_plotly_ozono.py
 
-echo "radiacion"
-#/home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/codigo/codespaces-jupyter_dam_01/src/write_plotly_radiacion.py
+#echo "radiacion"
+#/home/jcaubin/code/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/code/codespaces-jupyter_dam_01/src/write_plotly_radiacion.py
 
 
-echo "magnitudes"
-/home/jcaubin/codigo/codespaces-jupyter_dam_01/.env/bin/python /home/jcaubin/codigo/codespaces-jupyter_dam_01/src/write_plotly_magnitud.py
