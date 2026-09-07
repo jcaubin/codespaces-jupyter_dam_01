9c4392d97308441b59c73a395789f626d2334fb8
jcaubin <jcaubin@gmail.com>
2026-09-07 17:54:02 +0200

    datos de estaciones portus

--- src/portus_data.py
+++ src/portus_data.py
@@ -13,257 +13,6 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 DATA_DIR = PROJECT_ROOT / "data"
 DB_PATH = DATA_DIR / 'duck_test.db'
 
-ESTACIONES_PORTUS =  [
-	{
-		"id" : 1101,
-		"nombre" : "Boya de Pasaia II"
-	},
-	{
-		"id" : 1103,
-		"nombre" : "Boya Costera de Bilbao II"
-	},
-	{
-		"id" : 1117,
-		"nombre" : "Boya de Gijon"
-	},
-	{
-		"id" : 1135,
-		"nombre" : "Boya Costera de Abra-Zierbena"
-	},
-	{
-		"id" : 1239,
-		"nombre" : "Boya de Langosteira II"
-	},
-	{
-		"id" : 1250,
-		"nombre" : "Plataforma de Cortegada-CETMAR-INTECMAR-MG"
-	},
-	{
-		"id" : 1251,
-		"nombre" : "Plataforma de Rande-CETMAR-INTECMAR-MG"
-	},
-	{
-		"id" : 1253,
-		"nombre" : "Boya de A Guarda-CETMAR-INTECMAR-MG"
-	},
-	{
-		"id" : 1255,
-		"nombre" : "Boya de Ribeira-CETMAR-INTECMAR-MG"
-	},
-	{
-		"id" : 1256,
-		"nombre" : "Boya de Muros-CETMAR-INTECMAR-MG"
-	},
-	{
-		"id" : 1315,
-		"nombre" : "Boya de Sevilla-Guadalquivir"
-	},
-	{
-		"id" : 1414,
-		"nombre" : "Boya de Las Palmas Este"
-	},
-	{
-		"id" : 1421,
-		"nombre" : "Boya de Santa Cruz de Tenerife"
-	},
-	{
-		"id" : 1500,
-		"nombre" : "Boya de Tarifa"
-	},
-	{
-		"id" : 1504,
-		"nombre" : "Boya de Algeciras-Pta. Carnero"
-	},
-	{
-		"id" : 1512,
-		"nombre" : "Boya de Ceuta"
-	},
-	{
-		"id" : 1514,
-		"nombre" : "Boya de Málaga"
-	},
-	{
-		"id" : 1560,
-		"nombre" : "Boya de Melilla"
-	},
-	{
-		"id" : 1712,
-		"nombre" : "Boya de Tarragona"
-	},
-	{
-		"id" : 1731,
-		"nombre" : "Boya de Barcelona II"
-	},
-	{
-		"id" : 2136,
-		"nombre" : "Boya de Bilbao-Vizcaya"
-	},
-	{
-		"id" : 2242,
-		"nombre" : "Boya de Cabo de Peñas"
-	},
-	{
-		"id" : 2244,
-		"nombre" : "Boya de Estaca de Bares"
-	},
-	{
-		"id" : 2246,
-		"nombre" : "Boya de Villano-Sisargas"
-	},
-	{
-		"id" : 2248,
-		"nombre" : "Boya de Cabo Silleiro"
-	},
-	{
-		"id" : 2342,
-		"nombre" : "Boya de Golfo de Cádiz"
-	},
-	{
-		"id" : 2442,
-		"nombre" : "Boya de Gran Canaria"
-	},
-	{
-		"id" : 2446,
-		"nombre" : "Boya de Tenerife Sur"
-	},
-	{
-		"id" : 2548,
-		"nombre" : "Boya de Cabo de Gata"
-	},
-	{
-		"id" : 2610,
-		"nombre" : "Boya de Cabo de Palos"
-	},
-	{
-		"id" : 2630,
-		"nombre" : "Boya de Valencia"
-	},
-	{
-		"id" : 2720,
-		"nombre" : "Boya de Tarragona"
-	},
-	{
-		"id" : 2798,
-		"nombre" : "Boya de Cabo Begur"
-	},
-	{
-		"id" : 2801,
-		"nombre" : "Boya Bahía de Palma SOCIB"
-	},
-	{
-		"id" : 2820,
-		"nombre" : "Boya de Dragonera"
-	},
-	{
-		"id" : 2838,
-		"nombre" : "Boya de Mahón"
-	},
-	{
-		"id" : 3801,
-		"nombre" : "Mareografo de Andratx-SOCIB"
-	},
-	{
-		"id" : 3802,
-		"nombre" : "Mareografo de Pollença-SOCIB"
-	},
-	{
-		"id" : 3803,
-		"nombre" : "Mareografo de Sa Rapita-SOCIB"
-	},
-	{
-		"id" : 12110,
-		"nombre" : "Boya MONICAN01-IHP"
-	},
-	{
-		"id" : 12115,
-		"nombre" : "Boya MONICAN02-IHP"
-	},
-	{
-		"id" : 12120,
-		"nombre" : "Boya Raia01-IHP"
-	},
-	{
-		"id" : 12125,
-		"nombre" : "Boya Faro"
-	},
-	{
-		"id" : 21003,
-		"nombre" : "Boya Galway-MI"
-	},
-	{
-		"id" : 22091,
-		"nombre" : "Boya M2 E of Lambay-MI"
-	},
-	{
-		"id" : 22092,
-		"nombre" : "Boya M3 SW of Mizen Head-MI"
-	},
-	{
-		"id" : 22093,
-		"nombre" : "Boya M4 Donegal Bay-MI"
-	},
-	{
-		"id" : 22094,
-		"nombre" : "Boya M5 South East-MI"
-	},
-	{
-		"id" : 32001,
-		"nombre" : "Boya de Gascogne-UKMOMF"
-	},
-	{
-		"id" : 32029,
-		"nombre" : "Boya K1-UKMO"
-	},
-	{
-		"id" : 32052,
-		"nombre" : "Boya 62052-MF"
-	},
-	{
-		"id" : 32081,
-		"nombre" : "Boya K2-UKMO"
-	},
-	{
-		"id" : 32103,
-		"nombre" : "Barco Channel-UKMO"
-	},
-	{
-		"id" : 32105,
-		"nombre" : "Boya K4-UKMO"
-	},
-	{
-		"id" : 32107,
-		"nombre" : "Barco Seven Stones-UKMO"
-	},
-	{
-		"id" : 32163,
-		"nombre" : "Boya Brittany-UKMOMF"
-	},
-	{
-		"id" : 32301,
-		"nombre" : "Boya Aberporth-UKMO"
-	},
-	{
-		"id" : 32303,
-		"nombre" : "Boya Turbot Bank-UKMO"
-	},
-	{
-		"id" : 32304,
-		"nombre" : "Barco Sandettie-UKMO"
-	},
-	{
-		"id" : 32305,
-		"nombre" : "Barco Greenwich-UKMO"
-	},
-	{
-		"id" : 34045,
-		"nombre" : "Boya K5-UKMO"
-	},
-	{
-		"id" : 34046,
-		"nombre" : "Boya K7-UKMO"
-	}
-]
-
 
 def get_portus_current_data(station: int=2820) -> pd.DataFrame:
     headers = {
@@ -286,9 +35,10 @@ def get_portus_current_data(station: int=2820) -> pd.DataFrame:
 
 def store_portus_data(conn: duckdb.DuckDBPyConnection) -> None:
     try:
+        estaciones = get_estaciones_from_db(conn)
         fx_data = datetime.now()
         conn.execute("TRUNCATE TABLE PORTUS_CURRENT_DATA")
-        for estacion in ESTACIONES_PORTUS:
+        for estacion in estaciones:
             station_id = estacion["id"]
             df_portus = get_portus_current_data(station=station_id)
             df_portus['FX_DATA'] = fx_data
@@ -304,6 +54,14 @@ def store_portus_data(conn: duckdb.DuckDBPyConnection) -> None:
         print(f"Error al obtener los datos DE PORTUS: {e}")
         raise
 
+def get_estaciones_from_db(conn: duckdb.DuckDBPyConnection) -> list:	
+	try:
+		query = "SELECT id, nombre FROM PORTUS_ESTACIONES"
+		df_estaciones = conn.execute(query).fetchdf()
+		return df_estaciones.to_dict('records')
+	except Exception as e:
+		print(f"Error al obtener las estaciones de la base de datos: {e}")
+		raise	
 
 def process_portus_data(conn: duckdb.DuckDBPyConnection) -> None:
     try:
@@ -320,23 +78,6 @@ def process_portus_data(conn: duckdb.DuckDBPyConnection) -> None:
     except Exception as e:
         print(f"Error al procesar los datos de portus: {e}")
         raise
-
-
-
-def connect_db() -> duckdb.DuckDBPyConnection:
-    return duckdb.connect(str(DB_PATH))
-
-def download_and_store_portus_data() -> None:
-    conn = connect_db()
-    try:
-        store_portus_data(conn)
-        process_portus_data(conn)
-        store_portus_estaciones(conn)
-        print("Datos de portus descargados y procesados correctamente.")
-    except Exception as e:
-        print(f"Error al descargar y procesar los datos de portus: {e}")    
-    finally:
-        conn.close()
 
 def get_portus_estaciones() -> pd.DataFrame:
     magnitud = "WATER_TEMP"
@@ -366,6 +107,20 @@ def store_portus_estaciones(conn: duckdb.DuckDBPyConnection) -> None:
         print(f"Error al obtener los datos de estaciones de portus: {e}")
         raise
 
+def connect_db() -> duckdb.DuckDBPyConnection:
+    return duckdb.connect(str(DB_PATH))
+
+def download_and_store_portus_data() -> None:
+    conn = connect_db()
+    try:
+        store_portus_estaciones(conn) #descarga y guarda la lista de estaciones de portus en la tabla 'portus_estaciones'
+        store_portus_data(conn) #descarga y guarda los datos de portus en la tabla 'portus_current_data'
+        process_portus_data(conn) #procesa y guarda los datos de portus en la tabla 'portus'        
+        print("Datos de portus descargados y procesados correctamente.")
+    except Exception as e:
+        print(f"Error al descargar y procesar los datos de portus: {e}")    
+    finally:
+        conn.close()
 
 if __name__ == "__main__":
     download_and_store_portus_data()    
